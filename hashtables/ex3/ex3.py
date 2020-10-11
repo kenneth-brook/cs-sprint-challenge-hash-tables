1adc6bb4eefd67a9f957d6815c40587f7e5db3bd
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-10-11 19:25:12 -0400

    1-4 done

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,19 @@
+
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    cache = {}
+
+    for arr in arrays:
+        for item in arr:
+            if item in cache:
+                cache[item] += 1
+
+            else:
+                cache[item] = 1
+
+    for key in cache.keys():
+        if cache[key] == len(arrays):
+            result.append(key)
 
     return result
 
@@ -14,4 +25,5 @@ if __name__ == "__main__":
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
+    print(intersection(arrays))
     print(intersection(arrays))
